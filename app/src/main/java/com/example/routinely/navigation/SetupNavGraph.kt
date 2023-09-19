130987dabab2c75095a5a1efb71cc00036c439d2
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-09-18 23:51:18 -0300

    :sparkles: feat: Adding App Bars

--- app/src/main/java/com/example/routinely/navigation/SetupNavGraph.kt
+++ app/src/main/java/com/example/routinely/navigation/SetupNavGraph.kt
@@ -2,20 +2,20 @@ package com.example.routinely.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.routinely.changepassword.ForgotPasswordScreen
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.routinely.home.HomeScreen
-import com.example.routinely.login.CreateAccountScreen
 import com.example.routinely.login.LoginScreen
 import com.example.routinely.login.LoginViewModel
 
 @Composable
-fun NavGraph(
+fun SetupNavGraph(
     navController: NavHostController
 ) {
     NavHost(
@@ -32,6 +32,17 @@ fun NavGraph(
             },
             navigateToForgotPasswordScreen = {
                 navController.navigate(Screen.ForgotPasswordScreen.route)
+            },
+        )
+        homeScreenRoute(
+            onNotificationClicked = {
+//                navController.navigate()
+            },
+            onMenuClicked = {
+//                navController.navigate()
+            },
+            onNewTaskClicked = {
+//                navController.navigate()
             },
         )
     }
@@ -78,11 +89,22 @@ fun NavGraphBuilder.verificationCodeRoute(
 }
 
 fun NavGraphBuilder.homeScreenRoute(
-
+    onMenuClicked: () -> Unit,
+    onNotificationClicked: () -> Unit,
+    onNewTaskClicked: () -> Unit,
 ) {
     composable(route = Screen.HomeScreen.route) {
         HomeScreen(
-
+            onMenuClicked = { onMenuClicked() },
+            onNotificationClicked = { onNotificationClicked() },
+            onNewTaskClicked = { onNewTaskClicked() },
         )
     }
+}
+
+@Composable
+fun getCurrentDestination(navController: NavController): NavDestination? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    return navBackStackEntry?.destination
 }
