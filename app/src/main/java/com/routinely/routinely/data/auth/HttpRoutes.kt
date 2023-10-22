f25f0a59b87e3adc787f5aee52ffce7df1aac880
Jean Patrick Hartmann <32403781+hartmannjean@users.noreply.github.com>
2023-10-21 21:13:46 -0300

    Update HttpRoutes.kt

--- app/src/main/java/com/routinely/routinely/data/auth/HttpRoutes.kt
+++ app/src/main/java/com/routinely/routinely/data/auth/HttpRoutes.kt
@@ -1,7 +1,7 @@
 package com.routinely.routinely.data.auth
 
 object HttpRoutes {
-    private const val BASE_URL = "https://routinely-api-dev.onrender.com"
+    private const val BASE_URL = "xxxxxxxxxxxxxx.com"
     const val REGISTER = "$BASE_URL/auth/register"
     const val ADDTASK = "$BASE_URL/tasks"
     const val LOGIN = "$BASE_URL/auth"
