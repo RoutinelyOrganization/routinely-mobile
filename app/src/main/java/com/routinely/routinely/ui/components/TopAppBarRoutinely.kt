a68ef34e40be46351fb2f66cfa793a376b6347ba
Ruliam dos Santos de Oliveira <ruliam@proton.me>
2023-10-25 15:06:26 -0300

    :ok_hand: fix: Follow Design System for Menu (#56)
    * :ok_hand: fix: The menu is following the figma design
    
    * :ok_hand: fix: Menu close button works

--- app/src/main/java/com/routinely/routinely/ui/components/TopAppBarRoutinely.kt
+++ app/src/main/java/com/routinely/routinely/ui/components/TopAppBarRoutinely.kt
@@ -2,10 +2,14 @@ package com.routinely.routinely.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.Notifications
@@ -15,6 +19,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -22,8 +27,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.routinely.routinely.R
+import com.routinely.routinely.ui.crop
 import com.routinely.routinely.ui.theme.PurpleRoutinely
 import com.routinely.routinely.util.MenuItem
 
@@ -89,23 +96,47 @@ fun TopAppBarRoutinely(
                         tint = Color.White
                     )
                 }
-                DropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { onDismissMenu() },
-                    modifier = Modifier
-                        .width(156.dp)
-                        .background(color = Color.White)
+                MaterialTheme(
+                    shapes = MaterialTheme.shapes.copy(
+                        extraSmall = RoundedCornerShape(0.dp, 0.dp, 16.dp, 16.dp)
+                    )
                 ) {
-                    menuItems.forEach {
-                        DropdownMenuItem(
-                            text = { Text(it.text, color = PurpleRoutinely) },
-                            onClick = { it.onItemClick() },
-                            contentPadding = PaddingValues(start = 8.dp)
-                        )
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { onDismissMenu() },
+                        modifier = Modifier
+                            .width(156.dp)
+                            .background(color = Color.White)
+                            .crop(0.dp, 8.dp),
+//                        offset = DpOffset(10.dp, 20.dp)
+                        offset = DpOffset(0.dp, 8.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .background(Color.White)
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End
+                        ) {
+                            IconButton(
+                                onClick = { onDismissMenu() }
+                            ) {
+                                Icon(
+                                    painter = painterResource(R.drawable.ic_close),
+                                    contentDescription = "Teste",
+                                    tint = Color.Black
+                                )
+                            }
+                        }
+                        menuItems.forEach {
+                            DropdownMenuItem(
+                                text = { Text(it.text, color = PurpleRoutinely) },
+                                onClick = { it.onItemClick() },
+                                contentPadding = PaddingValues(start = 8.dp)
+                            )
+                        }
                     }
                 }
             }
-
         }
     )
 }
