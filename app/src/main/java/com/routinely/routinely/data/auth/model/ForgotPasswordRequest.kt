c89ea777cf224903f8bead3d8cc9aac6104cc85b
André Moraes Filho <softdev.andre@gmail.com>
2023-11-25 23:12:03 -0300

    ♻️ refactor: Refactored model accordingly with API structure

--- app/src/main/java/com/routinely/routinely/data/auth/model/ForgotPasswordRequest.kt
+++ app/src/main/java/com/routinely/routinely/data/auth/model/ForgotPasswordRequest.kt
@@ -4,8 +4,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ForgotPasswordRequest(
-    val name: String,
     val email: String,
-    val password: String,
-    val acceptedTerms: Boolean,
 )
