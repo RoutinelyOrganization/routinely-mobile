663c42e0d35e7c9f2627048ef23a049cb6c0f21e
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2024-03-13 13:15:33 -0300

    :sparkles: feat: Adding chucker

--- app/src/main/java/com/routinely/routinely/MainActivity.kt
+++ app/src/main/java/com/routinely/routinely/MainActivity.kt
@@ -1,10 +1,25 @@
 package com.routinely.routinely
 
 import android.annotation.SuppressLint
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.window.DialogProperties
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.compose.rememberNavController
 import com.routinely.routinely.data.core.Session
 import com.routinely.routinely.navigation.Screen
@@ -18,10 +33,15 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContent {
             RoutinelyTheme {
                 val navController = rememberNavController()
+                checkNotificationPolicyAccess(notificationManager, this)
                 SetupNavGraph(
                     navController = navController,
                     startDest = getStartDestination(),
@@ -32,5 +52,57 @@ class MainActivity : ComponentActivity() {
 
     private fun getStartDestination(): Screen {
         return if(session.getRememberLogin()) Screen.HomeScreen else Screen.SplashScreen
+    }
+}
+
+@Composable
+fun checkNotificationPolicyAccess(
+    notificationManager: NotificationManager,
+    context: Context
+): Boolean {
+    if (notificationManager.areNotificationsEnabled() || Build.VERSION.SDK_INT < 32) {
+        return true
+    } else {
+        PermissionDialog(context)
+    }
+    return false
+}
+
+@Composable
+fun PermissionDialog(context: Context) {
+    val openDialog = remember { mutableStateOf(true) }
+
+    if (openDialog.value) {
+        AlertDialog(
+            properties = DialogProperties(
+                usePlatformDefaultWidth = true
+            ),
+            title = { Text("Permissão necessária") },
+            text = {
+                Text("Para utilizar o Chucker no modo de Debug é necessário ativar as notificações do aplicativo no Android 13+")
+            },
+            onDismissRequest = { openDialog.value = true },
+
+            dismissButton = {
+                Button(
+                    onClick = {
+                        openDialog.value = false
+                    },
+                ) {
+                    Text(text = "Cancelar")
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    val uri: Uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        data = uri
+                    }
+                    openDialog.value = false
+                    startActivity(context, intent, null)
+                }) { Text(text = "Confirmar") }
+            },
+        )
     }
 }
--- app/src/main/java/com/routinely/routinely/data/core/coreModule.kt
+++ app/src/main/java/com/routinely/routinely/data/core/coreModule.kt
@@ -6,30 +6,69 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
+import com.routinely.routinely.BuildConfig
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.android.Android
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.gson.gson
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val coreModule = module {
-    single<HttpClient> {
-        HttpClient(Android){
-            install(Logging){
-                level = LogLevel.ALL
-                logger = Logger.ANDROID
-            }
+    single {
+        HttpClient(get<HttpClientEngine>()) {
+
             install(ContentNegotiation){
                 gson {
                     serializeNulls()
+                    setPrettyPrinting()
                 }
             }
+            install(DefaultRequest) {
+                header(HttpHeaders.ContentType, ContentType.Application.Json)
+            }
+            if(BuildConfig.DEBUG) {
+                install(Logging) {
+                    logger = Logger.ANDROID
+                    level = LogLevel.ALL
+                }
+            }
+        }
+
+    }
+
+    single<ChuckerInterceptor> {
+        ChuckerInterceptor.Builder(get())
+            .collector(
+                ChuckerCollector(
+                context = get(),
+                showNotification = true,
+                retentionPeriod = RetentionManager.Period.ONE_HOUR
+            )
+            )
+            .maxContentLength(250000L)
+            .redactHeaders(emptySet())
+            .alwaysReadResponseBody(false)
+            .build()
+    }
+
+    single<HttpClientEngine> {
+        OkHttp.create {
+            addNetworkInterceptor(get<ChuckerInterceptor>())
         }
     }
     single<DataStore<Preferences>> {
