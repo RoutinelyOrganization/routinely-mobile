76828e8bd1c3c86e0f5c244b4a7b3fc56643c7e0
CrozCamel <carlylecamelo@gmail.com>
2024-07-17 17:22:04 -0300

    corrections and updates

--- app/src/main/java/com/routinely/routinely/ui/components/FlatCalendarRoutinely.kt
+++ app/src/main/java/com/routinely/routinely/ui/components/FlatCalendarRoutinely.kt
@@ -1,6 +1,5 @@
 package com.routinely.routinely.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
