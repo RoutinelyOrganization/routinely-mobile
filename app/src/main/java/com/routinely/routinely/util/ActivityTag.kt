d3e9865f45daf68647ca23d84831938373453985
CrozCamel <carlylecamelo@gmail.com>
2024-11-26 14:43:23 -0300

    Adicionando ActivityTag devido o erro

--- app/src/main/java/com/routinely/routinely/util/ActivityTag.kt
+++ app/src/main/java/com/routinely/routinely/util/ActivityTag.kt
@@ -6,17 +6,18 @@ import kotlinx.parcelize.Parcelize
 sealed class ActivityTag(stringId: Int, apiString: String) : TaskFields(stringId, apiString) {
 
     @Parcelize
-    data object Task : ActivityTag(R.string.text_tag_activity, "Activity")
-
+    data object Task : ActivityTag(R.string.text_tag_task, "Task")
     @Parcelize
     data object Habit : ActivityTag(R.string.text_tag_habit, "Habit")
-
     @Parcelize
     data object Project : ActivityTag(R.string.text_tag_project, "Project")
 
-    @Parcelize
-    data object AllActivity : ActivityTag(R.string.text_tag_activities, "All_Activity")
+}
 
-    @Parcelize
-    data object Completed : ActivityTag(R.string.text_tag_completed, "Completed")
+fun ActivityTag.toStringId(): Int {
+    return this.stringId
+}
+
+fun fromStringId(stringId: Int): ActivityTag {
+    return TaskFields.getTaskFieldByStringId<ActivityTag>(stringId)
 }
